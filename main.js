8163f0b8759b7840914e12ce9859390c8ab8b746
Karen Nguyen <knguyen4184@yahoo.com>
2019-10-01 08:28:56 -0600

    Finish html layout and style

---
+++ main.js
@@ -0,0 +1,30 @@
+var searchInput = document.querySelector(".search-bar");
+var searchButton = document.querySelector(".search-button");
+var titleInput = document.querySelector(".title-input");
+var captionInput = document.querySelector(".caption-input");
+var chooseFileButton = document.getElementById("");
+var addToAlbumButton = document.querySelector(".add-to-album-button")
+var photoCardTitle = document.getElementById("photo-title");
+var photoCardCaption = document.getElementById("caption-title");
+var deleteCard = document.getElementById("remove-card");
+var favoriteCard = document.getElementById("favorite-card");
+var parentContainer = document.querySelector(".main-empty");
+
+searchButton.addEventListner("click" );
+parentContainer.addEventListner("click" );
+
+function onSearchClick() {
+
+}
+
+function onCardClick() {
+
+}
+
+function instantiate() {
+
+}
+
+function createCard() {
+  
+}
